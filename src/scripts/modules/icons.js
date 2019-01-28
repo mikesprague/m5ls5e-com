79f693dbe195a50c955cac1a71de3c8cd3055614
Michael Sprague <mikesprague@gmail.com>
2019-01-28 14:55:16 -0500

    :building_construction: More build system updates
    Fixes deploy

--- src/scripts/modules/icons.js
+++ src/scripts/modules/icons.js
@@ -1,12 +1,9 @@
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import {
-  faGithub, faTwitter, faLinkedin
-} from "@fortawesome/free-brands-svg-icons";
-// import {
-//   faTint, faCode,
-//   faSun as faSunLight, faMoonStars, faCloudRain, faCloudSnow, faCloudSleet, faWind as faWIndLight,
-//   faFog, faClouds, faCloudsSun, faCloudsMoon, faCloudHail, faHurricane, faThunderstorm, faTornado
-// } from "@fortawesome/pro-light-svg-icons";
+  faGithub,
+  faTwitter,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 export function initFontAwesomeIcons() {
   library.add(
