326b64dc6d6b0e2963fe92aaa7329bd056d9daa3
Michael Sprague <mikesprague@gmail.com>
2022-09-07 19:32:50 -0400

    :wrench: Improve config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-
-const isDev = process.env.NODE_ENV !== 'production';
 
 export default defineConfig({
   root: 'src',
@@ -9,10 +6,8 @@ export default defineConfig({
     outDir: '../dist',
     rollupOptions: {
       input: {
-        main: isDev ? resolve(__dirname, 'index.html') : './index.html',
-        nested: isDev
-          ? resolve(__dirname, 'thanksgiving-1984/index.html')
-          : './thanksgiving-1984/index.html',
+        main: './src/index.html',
+        nested: './src/thanksgiving-1984/index.html',
       },
     },
   },
