17c51b993d496b7f3bffa10ff13a81f2508d1202
Michael Sprague <mikesprague@gmail.com>
2022-11-02 16:24:13 -0400

    :sparkles: Add quotes endpoint (uses zenquotes api)

---
+++ functions/api/quotes.js
@@ -0,0 +1,62 @@
+export const onRequestGet = async (context) => {
+  const CACHE_NAME = 'quotes';
+  const { request } = context;
+
+  const cache = await caches.open(CACHE_NAME);
+
+  const cachedData = await cache.match(request);
+
+  if (cachedData) {
+    console.log('🚀 using cached data!');
+
+    const returnData = await cachedData.json();
+
+    return new Response(JSON.stringify(returnData), cachedData);
+  }
+
+  console.log('😢 no cache, fetching new data');
+
+  const normalizeQuoteData = (apiData) => {
+    const returnData = apiData.map((quoteData) => {
+      const quoteExcerpt = quoteData.q;
+      const quoteHtml = quoteData.h;
+      const quoteAuthor = quoteData.a;
+
+      return {
+        quoteExcerpt,
+        quoteHtml,
+        quoteAuthor,
+      };
+    });
+
+    return returnData;
+  };
+
+  const quotesData = await fetch('https://zenquotes.io/api/quotes/')
+    .then(async (response) => {
+      const returnData = await response.json();
+
+      return normalizeQuoteData(returnData);
+    })
+    .catch((error) => {
+      console.error(error);
+
+      return new Response(JSON.stringify(error), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    });
+
+  const response = new Response(JSON.stringify(quotesData), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'max-age=3600, s-maxage=3600',
+    },
+  });
+
+  // cache data;
+  context.waitUntil(cache.put(request, response.clone()));
+
+  return response;
+};
