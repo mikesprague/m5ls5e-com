62289650d80f4b14a8ad1fc48abeb9777cb2aec4
Michael Sprague <mikesprague@gmail.com>
2022-11-13 08:54:32 -0500

    :recycle: Update endpoints

--- functions/api/github-trending-repos.js
+++ functions/api/github-trending-repos.js
@@ -1,10 +1,8 @@
-import cheerio from 'cheerio';
-
 export const onRequestGet = async (context) => {
   const CACHE_NAME = 'github-trending-repos';
   const { request } = context;
 
-  let cache = await caches.open(CACHE_NAME);
+  const cache = await caches.open(CACHE_NAME);
 
   const cachedData = await cache.match(request);
 
@@ -18,80 +16,26 @@ export const onRequestGet = async (context) => {
 
   console.log('😢 no cache, fetching new data');
 
+  const { url } = context.request;
+
+  const urlParams = new URL(url).searchParams;
+
+  const healthcheck = urlParams.get('healthcheck');
+
+  if (healthcheck) {
+    return new Response(JSON.stringify('API is up and running'), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
   const postsData = await fetch(
-    'https://github.com/trending?spoken_language_code=en',
+    'https://mikesprague.github.io/api/github-trending-repos.json',
   )
     .then(async (response) => {
-      const markup = await response.text();
-      const rowSelector = 'article.Box-row';
-      const linkTitleSelector = 'h1 > a';
-      const descriptionSelector = 'p';
-      const languageSelector = 'div.f6.color-fg-muted.mt-2';
-      const starsSelector = `${languageSelector} > a:nth-child(2)`;
-      const forksSelector = `${languageSelector} > a:nth-child(3)`;
-      const starsTodaySelector = `${languageSelector} > span.d-inline-block.float-sm-right`;
-      const languageColorSelector = `${languageSelector} > span.d-inline-block.ml-0.mr-3 > .repo-language-color`;
-      const languageNameSelector = `${languageSelector} > span.d-inline-block.ml-0.mr-3 > span:nth-child(2)`;
-      const $ = cheerio.load(markup);
-      const returnData = [];
+      const returnData = await response.json();
 
-      $(rowSelector).each((i, elem) => {
-        const title = $(elem).find(linkTitleSelector).attr('href').substring(1);
-        const link = `https://github.com${$(elem)
-          .find(linkTitleSelector)
-          .attr('href')}`;
-        const description = $(elem)
-          .find(descriptionSelector)
-          .text()
-          .replace(/\r?\n|\r/, '')
-          .trim();
-        const forksLink = `https://github.com${$(elem)
-          .find(forksSelector)
-          .attr('href')}`;
-        const starsLink = `https://github.com${$(elem)
-          .find(starsSelector)
-          .attr('href')}`;
-        const stars = $(elem)
-          .find(starsSelector)
-          .text()
-          .replace(/\r?\n|\r/, '')
-          .trim();
-        const forks = $(elem)
-          .find(forksSelector)
-          .text()
-          .replace(/\r?\n|\r/, '')
-          .trim();
-        const starsToday = $(elem)
-          .find(starsTodaySelector)
-          .text()
-          .replace(/\r?\n|\r/, '')
-          .trim();
-        const languageStyle = $(elem).find(languageColorSelector).attr('style')
-          ? $(elem).find(languageColorSelector).attr('style')
-          : null;
-        const languageName = languageStyle
-          ? $(elem)
-              .find(languageNameSelector)
-              .text()
-              .replace(/\r?\n|\r/, '')
-              .trim()
-          : null;
-
-        returnData.push({
-          title,
-          description,
-          stars,
-          starsLink,
-          forks,
-          forksLink,
-          starsToday,
-          languageStyle,
-          languageName,
-          link,
-        });
-      });
-
-      return returnData;
+      return returnData.data;
     })
     .catch((error) => {
       console.error(error);
@@ -115,3 +59,4 @@ export const onRequestGet = async (context) => {
 
   return response;
 };
+
--- functions/api/national-day-calendar.js
+++ functions/api/national-day-calendar.js
@@ -1,12 +1,8 @@
-import cheerio from 'cheerio';
-
-// data source: https://nationaldaycalendar.com
-
 export const onRequestGet = async (context) => {
   const CACHE_NAME = 'national-day-calendar';
   const { request } = context;
 
-  let cache = await caches.open(CACHE_NAME);
+  const cache = await caches.open(CACHE_NAME);
 
   const cachedData = await cache.match(request);
 
@@ -20,12 +16,13 @@ export const onRequestGet = async (context) => {
 
   console.log('😢 no cache, fetching new data');
 
-  const allData = [];
-  const pageData = await fetch('https://nationaldaycalendar.com/')
+  const postsData = await fetch(
+    'https://mikesprague.github.io/api/national-day.json',
+  )
     .then(async (response) => {
-      const markup = await response.text();
+      const returnData = await response.json();
 
-      return markup;
+      return returnData.data;
     })
     .catch((error) => {
       console.error(error);
@@ -35,46 +32,8 @@ export const onRequestGet = async (context) => {
         headers: { 'Content-Type': 'application/json' },
       });
     });
-  const $ = cheerio.load(pageData);
-  const groupSelector = 'div.sep_month_events';
-  const groupData = $(groupSelector);
-  const days = $(groupData).find(
-    '.eventon_list_event.evo_eventtop.scheduled.event',
-  );
 
-  for await (const day of $(days)) {
-    const title = $(day).find('span.evcal_event_title').text().trim();
-    const link = $(day).find('a').first().attr('href').trim();
-    // console.log(title, link);
-    const descriptionData = await fetch(link)
-      .then(async (response) => {
-        const markup = await response.text();
-
-        return markup;
-      })
-      .catch((error) => {
-        console.error(error);
-
-        return new Response(JSON.stringify(error), {
-          status: 500,
-          headers: { 'Content-Type': 'application/json' },
-        });
-      });
-    const selector = '#ff-main-container > main > article > section';
-    const $desc = cheerio.load(descriptionData);
-    const description = $desc(selector).find('h2 ~ p').first().text().trim();
-    // console.log(description);
-
-    if (title && link && description) {
-      allData.push({
-        title,
-        link,
-        description,
-      });
-    }
-  }
-
-  const response = new Response(JSON.stringify(allData), {
+  const response = new Response(JSON.stringify(postsData), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
