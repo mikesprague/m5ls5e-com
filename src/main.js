b95e34e5e108fb35dfc6957c3a3adde6194ce376
Michael Sprague <mikesprague@gmail.com>
2026-09-09 15:44:03 -0400

    refactor: misc cleanup and dependency updates

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import 'bulma/bulma.sass';
+import '../node_modules/bulma/css/bulma.min.css';
 import { initFontAwesomeIcons } from './modules/icons.js';
 
-import './scss/styles.scss';
+import './css/styles.css';
 
 initFontAwesomeIcons();
 console.info('🙊 If you know, you know');
